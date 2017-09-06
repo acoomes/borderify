e87ecbe638a08a651565ace17eb4c7f4885d28ab
Andrew Coomes <86drift@gmail.com>
2017-09-05 19:13:11 -0700

    Initial commit

---
+++ borderify.js
@@ -0,0 +1,4 @@
+document.body.style.border = "5px solid red";
+document.body.style.borderRadius = "50px";
+document.getElementById("content").style.border = "5px solid black";
+document.getElementById("content").style.borderRadius = "20px";
